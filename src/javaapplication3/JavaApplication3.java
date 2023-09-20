d174d7a65c3cdf7be75e5c01c5b6d7271d4779ab
VorosDominik <medoxidhun@gmail.com>
2023-09-20 12:51:47 +0200

    metodus

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -9,9 +9,14 @@ public class JavaApplication3 {
 
     
     public static void main(String[] args) {
+        metodus();
         for (int i = 0; i < 10; i++) {
             
         }
+    }
+
+    private static void metodus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
